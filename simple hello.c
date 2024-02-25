8f87cb1b30728c2a1e0432eec61c011e9c123dbf
Velmurugan Pandiyan <pvelmurugaa@gmail.com>
2024-02-25 17:45:31 +0400

    simple c prog

---
+++ simple hello.c
@@ -0,0 +1,8 @@
+#include<stdio.h> //header file which include standard input/output functions
+
+int main() //every prog must contain one main func
+
+{
+    printf("Hello, World\n"); //printf - prints given input within func
+    return(0);
+}
